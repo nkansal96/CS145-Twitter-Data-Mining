60f8388059e5e6a332599f9247c2c36678cb56ce
Nikhil Kansal <nkansal96@gmail.com>
2017-11-03 11:43:13 -0700

    add sample run script for submission

---
+++ sample_run.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+FILE=/tmp/test_tweet.bin
+
+rm -rf $FILE
+
+pip install TwitterAPI
+python app.py --scrape --file $FILE --max_tweets 3
+
+if [ ! $? -eq 0 ]; then
+	echo 'Error running the scraper!'
+	rm -rf $FILE
+	exit 1
+fi
+
+python app.py --dump --file $FILE
+
+if [ ! $? -eq 0 ]; then
+	echo 'Error running the scraper!'
+	rm -rf $FILE
+	exit 1
+fi
