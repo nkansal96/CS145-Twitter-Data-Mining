73cbd00aa9cd8c8dfc81008eb2d6415db962f8b9
Nikhil Kansal <nkansal96@gmail.com>
2017-11-03 12:25:41 -0700

    changes according to TA  feedback

--- sample_run.sh
+++ sample_run.sh
@@ -3,20 +3,6 @@
 FILE=/tmp/test_tweet.bin
 
 rm -rf $FILE
-
-pip install TwitterAPI
 python code/app.py --scrape --file $FILE --max_tweets 3
-
-if [ ! $? -eq 0 ]; then
-	echo 'Error running the scraper!'
-	rm -rf $FILE
-	exit 1
-fi
-
 python code/app.py --dump --file $FILE
-
-if [ ! $? -eq 0 ]; then
-	echo 'Error running the scraper!'
-	rm -rf $FILE
-	exit 1
-fi
+rm -rf $FILE
