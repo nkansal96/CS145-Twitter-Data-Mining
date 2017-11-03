bd3a343fb4a0d1ef6093e3616a2b82b8ab7947c0
Nikhil Kansal <nkansal96@gmail.com>
2017-11-03 11:35:11 -0700

    simplify options, add Tweet class

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from TwitterAPI import TwitterAPI
-from twitter_data import TwitterData
+from twitter_data import TwitterData, Tweet
 from passwords import *
-import argparse, pprint, math
+import argparse, pprint, math, json
 
 def scrape_tweets(file_loc, max_tweets):
 	""" Scrape tweets and store into a file """
@@ -11,30 +11,25 @@ def scrape_tweets(file_loc, max_tweets):
 
 	stream = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
 	for tweet in stream:
-		td.add_data(tweet)
+		td.add_tweet(tweet)
 		count += 1
 		if count >= max_tweets:
 			break;
 
-def dump_data(file_loc, pretty):
+def dump_data(file_loc):
 	""" Dumps the data stored in the file """
 	td = TwitterData(file_loc)
-	if pretty:
-		pprint.pprint(td.get_data())
-	else:
-		print(td.get_data())
-
+	print(json.dumps(td.get_tweets(), sort_keys=True, indent=2))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='CS 145 Twitter Data Mining Project')
 	parser.add_argument('--file', help='Location to store/retrieve tweets (default ./data.bin)', default='data.bin')
 	parser.add_argument('--max_tweets', help='The maximum number of tweets to crawl (default is (2 ** 63 - 1))', type=int, default=(2 ** 63 - 1))
 	parser.add_argument('--scrape', help='Start scraping tweets from twitter', action='store_true')
-	parser.add_argument('--raw_dump', help='Dump the raw data from the given file to stdout', action='store_true')
-	parser.add_argument('--pretty_dump', help='Dump the data prettified from the given file to stdout', action='store_true')
+	parser.add_argument('--dump', help='Dump the data from the given file to stdout in JSON format', action='store_true')
 
 	args = parser.parse_args()
 	if args.scrape:
 		scrape_tweets(args.file, args.max_tweets)
-	if args.raw_dump or args.pretty_dump:
-		dump_data(args.file, args.pretty_dump)
+	if args.dump:
+		dump_data(args.file)
--- twitter_data.py
+++ twitter_data.py
@@ -1,33 +1,41 @@
-import pickle, sys
+import pickle, sys, json
 
 class TwitterData(object):
 	""" Keep track of twitter data in a serialized fashion """
 	def __init__(self, file_loc):
 		""" Construct the object. Reads the given file_loc for
-    any existing data. If it exists and is unpickleable, it
+		any existing data. If it exists and is unpickleable, it
 		loads it. Otherwise it uses an empty list
 		"""
-		self.data = []
+		self.tweets = []
 		self.file_loc = file_loc
 		try:
-			sys.stdout.write("Attempting to read stored data from '{}'... ".format(file_loc))
-			sys.stdout.flush()
+			sys.stderr.write("Attempting to read stored tweets from '{}'... ".format(file_loc))
+			sys.stderr.flush()
 			with open(file_loc, 'rb') as f:
-				self.data = pickle.load(f)
-				sys.stdout.write("Read {} tweets.\n".format(len(self.data)))
+				self.tweets = pickle.load(f)
+				sys.stderr.write("Read {} tweets.\n".format(len(self.tweets)))
 		except:
 			sys.stderr.write("\n --> File not found or file not unpickleable -- using empty dataset\n")
 
-	def add_data(self, data):
-		""" Adds a data point to the object """
-		self.data.append(data)
+	def add_tweet(self, tweet):
+		""" Adds a tweet to the object """
+		self.tweets.append(tweet)
 		self.serialize()
 
-	def get_data(self):
-		""" Returns all data stored in the object """
-		return self.data
+	def get_tweets(self):
+		""" Returns all tweets stored in the object """
+		return self.tweets
 
 	def serialize(self):
 		""" Serializes the object. Writes its data to a file """
 		with open(self.file_loc, 'wb') as f:
-			pickle.dump(self.data, f)
+			pickle.dump(self.tweets, f)
+
+class Tweet(json.JSONEncoder):
+	""" One actual tweet object -- we can methods to transform data here """
+	def __init__(self, tweet):
+		self.tweet = tweet
+
+	def __repr__(self):
+		return json.loads(self.tweet)
