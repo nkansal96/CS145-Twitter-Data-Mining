8025008cad35c4c0dd0dedec033e6a620f9a708c
kianadk <kianadkavoosi@gmail.com>
2017-12-02 14:08:56 -0800

    filter tweets for key words

--- code/analyze.py
+++ code/analyze.py
@@ -1,19 +1,30 @@
-import argparse, json
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import argparse, json, pprint, re, string
 
 def analyze(file_loc):
   f = open(file_loc)
   tweets = json.load(f)
 
+  stop = set(stopwords.words('english'))
+  stop.update(['#', '@'])
+
   tweet_dict = {};
 
-  for tweet in tweets:
-    for hashtag in tweet['entities']['hashtags']:
-      if hashtag['text'] in tweet_dict:
-        tweet_dict[hashtag['text']].append(tweet['text'])
-      else:
-        tweet_dict[hashtag['text']] = [tweet['text']]
+  stemmer = PorterStemmer()
 
-  print(tweet_dict.keys())
+  for tweet in tweets:
+    text = tweet['text'].lower();
+    text = re.sub(r'http\S+', '', text)
+    keywords = word_tokenize(text)
+    keywords = [word for word in keywords if word not in stop]
+    keywords = [stemmer.stem(word) for word in keywords]
+    letters = re.compile('^[a-z0-9]+$')
+    keywords = [word for word in keywords if letters.match(word)]
+    tweet_dict[text] = set(keywords)
+
+  pprint.pprint(tweet_dict)
 
 
 
