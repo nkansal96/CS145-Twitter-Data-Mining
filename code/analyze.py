f142dd5260d726db37116a2af72a57c7ee54e150
kianadk <kianadkavoosi@gmail.com>
2017-12-02 14:57:00 -0800

    break into helper functions

--- code/analyze.py
+++ code/analyze.py
@@ -1,14 +1,20 @@
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
-import argparse, json, pprint, re, string
+import argparse, json, pprint, re
 
 def analyze(file_loc):
   f = open(file_loc)
   tweets = json.load(f)
 
+  tweet_dict = get_keywords(tweets)
+  cluster(list(tweet_dict.keys()), list(tweet_dict.values()))
+
+def cluster(tweets, keywords):
+  return tweets
+
+def get_keywords(tweets):
   stop = set(stopwords.words('english'))
-  stop.update(['#', '@'])
 
   tweet_dict = {};
 
@@ -24,10 +30,14 @@ def analyze(file_loc):
     keywords = [word for word in keywords if letters.match(word)]
     tweet_dict[text] = set(keywords)
 
-  pprint.pprint(tweet_dict)
+  return tweet_dict
 
-
-
+def get_distance(s1, s2):
+  distance = 0
+  for word in s1:
+    if word in s2:
+      distance += 1
+  return distance
 
 
 if __name__ == '__main__':
